3f23b934ef3141cc2d36c39026a394309dca9cce
Ernesto Canquiz <fdsoil123@gmail.com>
2017-06-26 10:34:05 -0400

    first commit

---
+++ relocate.js
@@ -0,0 +1,21 @@
+/** Ejecuta un sumit a una página específica, pasando los valores suministrados bajo el método POST.
+* @param page (string) Representa la ruta y el archivo de la página a donde se envía el sumit.
+* @param params (array) Lista de valores que se enviarán por el método POST.*/
+function relocate( page, params )
+{
+    var body = document.body;
+    form=document.createElement( 'form' ); 
+    form.method = 'POST'; 
+    form.action = page;
+    form.name = 'jsform';
+    for ( index in params ) {
+        var input = document.createElement( 'input' );
+	input.type = 'hidden';
+	input.name = index;
+	input.id = index;
+	input.value = params[ index ];
+	form.appendChild( input );
+    }	  		  			  
+    body.appendChild( form );
+    form.submit();
+}
---
+++ submit.js
@@ -0,0 +1,15 @@
+function submitInsert()
+{
+    relocate( 'index.php', {} ); 
+}
+
+function submitEdit( val1, val2, val3 )
+{
+    relocate( 'index.php', { 'id' : val1, 'reg' : val2 , 'des' : val3 } );
+}
+
+function submitDelete( val )
+{
+    if ( confirm( '¿Desea Eliminar Este Registro?' ) )    
+        relocate( 'index.php', { 'id' : val } );
+}
