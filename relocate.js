1d3596e903357a978c58bb466ebdf2ea2036649e
Ernesto Canquiz <fdsoil123@gmail.com>
2017-07-04 09:46:31 -0400

    Actualizando

--- relocate.js
+++ relocate.js
@@ -4,18 +4,19 @@
 function relocate( page, params )
 {
     var body = document.body;
-    form=document.createElement( 'form' ); 
-    form.method = 'POST'; 
+    form = document.createElement( "form" );
+    form.method = "POST";
     form.action = page;
-    form.name = 'jsform';
+    form.name = "jsform";
     for ( index in params ) {
-        var input = document.createElement( 'input' );
-	input.type = 'hidden';
-	input.name = index;
-	input.id = index;
-	input.value = params[ index ];
-	form.appendChild( input );
-    }	  		  			  
+        var input = document.createElement( "input" );
+        input.type = "hidden";
+        input.name = index;
+        input.id = index;
+        input.value = params[ index ];
+        form.appendChild( input );
+    }
     body.appendChild( form );
     form.submit();
 }
+
--- submit.js
+++ submit.js
@@ -1,15 +1,16 @@
 function submitInsert()
 {
-    relocate( 'index.php', {} ); 
+    relocate( "index.php", {} );
 }
 
 function submitEdit( val1, val2, val3 )
 {
-    relocate( 'index.php', { 'id' : val1, 'reg' : val2 , 'des' : val3 } );
+    relocate( "index.php", { "id" : val1, "reg" : val2, "des" : val3 } );
 }
 
 function submitDelete( val )
 {
-    if ( confirm( '¿Desea Eliminar Este Registro?' ) )    
-        relocate( 'index.php', { 'id' : val } );
+    if ( confirm( "¿Desea Eliminar Este Registro?" ) )
+        relocate( "index.php", { "id" : val } );
 }
+
